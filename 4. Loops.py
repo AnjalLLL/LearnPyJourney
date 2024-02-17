7109c054ed3627d8f15e4e7a30b4a781075124cd
AnjalLLL <anjalghimire890@gmail.com>
2024-02-17 19:57:16 +0545

    files

--- 4. Loops.py
+++ 4. Loops.py
@@ -1,9 +1,18 @@
 def whileLoop():
-    print("10 intergers start from 1")
-    i=1
-    while i<=10:
+    print("Printing numbers from 10 to 1")
+    i=10
+    while i != 0 :
         print(i)
-        i += 1
+        i -=1
 
 def forLoop():
-    
+    print("Printing 1st 10 natural numbers")
+    for i in range(10):
+        print(i)
+
+def main():
+    whileLoop()
+    forLoop()
+
+main()
+
