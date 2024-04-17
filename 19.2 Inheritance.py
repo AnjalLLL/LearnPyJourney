f809a7aa0b6b330c674c6474ea984aa2ebf710a1
AnjalLLL <anjalghimire890@gmail.com>
2024-04-17 23:45:18 +0545

    Updated

--- 19.2 Inheritance.py
+++ 19.2 Inheritance.py
@@ -1,12 +1,32 @@
 class Student:
-    def __init__(self,semester,name) :
-        
-        
-
+    def __init__(self,name) :
+        self.name = name
+    
+    def display(self):
+        print(f"My name is {self.name}")
+    
 class firstSemester(Student):
-    def __init__(self, semester, name):
-        super().__init__(semester, name)
+    def __init__(self, name, course):
+        super().__init__(name)
+        self.name= name
+        self.course= course
+    
+    def FisrtSemStudent(self):
+        print(f"{self.name} from {self.course}")
 
 class secondSemester(Student):
-    def __init__(self, semester, name):
-        super().__init__(semester, name)
+    def __init__(self, name, course):
+        super().__init__(name)
+        self.name = name
+        self.course = course
+
+def main():
+    student = Student("Anjal")
+    fristsem = firstSemester("Ashmita","Bsc.csit")
+    secondsem = secondSemester("Sita","BCA")
+    student.display()
+    fristsem.FisrtSemStudent()
+
+if __name__ == "__main__":
+    main()
+
